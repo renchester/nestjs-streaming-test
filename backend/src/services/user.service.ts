450bde07915a1448c98b1009cf001f004f677e9e
Renchester Ramos <renchesterjramos@gmail.com>
2023-08-26 10:04:23 +0800

    Create user service module

---
+++ backend/src/services/user.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/model/user.schema';
+import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async signup(user: User): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(user.password, salt);
+
+    const reqBody = {
+      fullName: user.fullname,
+      email: user.email,
+      password: hash,
+    };
+
+    const newUser = new this.userModel(reqBody);
+    return newUser.save();
+  }
+
+  async signIn(user: User, jwt: JwtService): Promise<any> {
+    const foundUser = await this.userModel
+      .findOne({ email: user.email })
+      .exec();
+
+    if (!foundUser) {
+      return new HttpException(
+        'Incorrect username or password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const { password } = foundUser;
+
+    if (bcrypt.compare(user.password, password)) {
+      const payload = { email: user.email };
+
+      return {
+        token: jwt.sign(payload),
+      };
+    }
+
+    return new HttpException(
+      'Incorrect username or password',
+      HttpStatus.UNAUTHORIZED,
+    );
+  }
+
+  async getOne(email: string): Promise<User> {
+    return await this.userModel.findOne({ email });
+  }
+}
