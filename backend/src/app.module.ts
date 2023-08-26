7c0309986f120ed98243e2c3eb7163bdb86897cb
Renchester Ramos <renchesterjramos@gmail.com>
2023-08-26 10:05:01 +0800

    Add imported modules to AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { secret } from './utils/constants';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Stream')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename: (req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}-${Date.now()}.${ext}`);
+        },
+      }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+    }),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
