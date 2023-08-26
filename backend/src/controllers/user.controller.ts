0272fd34665a9a371d6cd8327d8791edbdc2c321
Renchester Ramos <renchesterjramos@gmail.com>
2023-08-26 09:22:47 +0800

    Create initial user controller with signup functions

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  UploadedFiles,
+  Put,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { User } from 'src/model/user.schema';
+import { UserService } from 'src/model/user.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Controller('/api/v1/user')
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  @Post('/signup')
+  async Signup(@Res() response, @Body() user: User) {
+    const newUser = await this.userService.signup(user);
+
+    return response.status(HttpStatus.CREATED).json({ newUser });
+  }
+
+  @Post('/signin')
+  async SignIn(@Res() response, @Body() user: User) {
+    const token = await this.userService.signin(user, this.jwtService);
+
+    return response.status(HttpStatus.OK).json(token);
+  }
+}
